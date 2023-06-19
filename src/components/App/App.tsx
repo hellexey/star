1b6ed5e8d0eb220737bfa81a23ba470a565c3b7b
lesha <hellexey@gmail.com>
2023-06-19 18:35:13 +0500

    чет поделал

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+interface Person {
+  name: string
+  height: string
+  mass: string
+}
+const App: React.FC = () => {
+  const [data, setData] = useState<Person | null>(null)
 
-const App = () => {
+  const getResource = async (url: string) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setData(data)
+  }
+
+  const handleClick = () => {
+    getResource('https://swapi.dev/api/people/1')
+  }
+
   return (
     <div>
-      <h1>Star DB</h1>
+      <button onClick={handleClick}>Fetch</button>
+      {data && (
+        <div>
+          <p>Name: {data.name}</p>
+          <p>Height: {data.height}</p>
+          <p>Mass: {data.mass}</p>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/components/test/Test.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const TestFetch: React.FC = () => {
+  return <div></div>
+}
